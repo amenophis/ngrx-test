93552cad7579e12e2972d8203f1dbfcb7e8730a4
Jérémy LEHERPEUR <jeremy@leherpeur.net>
2017-07-26 13:19:33 +0200

    Test redux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Store } from '@ngrx/store';
+import { LOAD } from './counter';
+
+export interface AppState {
+    massifs: Array<Massif>;
+}
+
+export interface Massif {
+    id: string;
+    name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    massifs$: Observable<any>;
+
+    constructor(private store: Store<AppState>, private http: Http) {
+        this.http.get('https://api.hohey.fr/massifs.json')
+            .map(res => res.json())
+            .map(payload => ({ type: LOAD, payload }))
+            .subscribe((action) => {
+                console.log(action);
+                this.store.dispatch(action);
+            });
+    }
+
+    ngOnInit() {
+        this.massifs$ = this.store.select(state => state.massifs);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { counterReducer } from './counter';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 5
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
